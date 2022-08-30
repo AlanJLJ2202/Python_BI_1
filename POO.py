036a09a6826e916e186a8971345e67e0ae802fab
AlanJLJ2202 <lrs19110248@purisima.tecnm.mx>
2022-08-30 08:07:39 -0500

    POO (#4)

---
+++ POO.py
@@ -0,0 +1,62 @@
+# Divide y venceras
+
+# Herencia, polimorfismo, abstraccion y encapsulamiento
+
+class Perro:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def comer(self):
+        print("Comiendo")
+
+    def dormir(self):
+        print("Durmiendo")
+
+    def correr(self):
+        print("Corriendo")
+
+
+perro1 = Perro("Pingo", 6)
+print(perro1.nombre)
+
+perro2 = Perro("Firulais", 2)
+print(perro2.nombre)
+
+
+class Animal:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def comer(self):
+        pass
+
+    def dormir(self):
+        pass
+
+
+# Herencia
+class Gato(Animal):
+    def comer(self):
+        print(self.nombre, "Comiendo")
+
+
+gato1 = Gato("Teodoro", 12)
+gato1.comer()
+
+
+# Polimorfismo
+# Multiples objetos que tendran comportamientos similares.
+# Metodos que se llaman igual, pero dependiendo del objeto se adaptara a su comportamiento
+
+class Pajaro(Animal):
+    def comer(self):
+        print(self.nombre, "Comiendo")
+
+    def volar(self):
+        print(self.nombre, "volando pa")
+
+
+pajaro1 = Pajaro("Piolin", 2)
+pajaro1.comer()
